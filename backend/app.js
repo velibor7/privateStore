8907e847b313c0b15e88b3ab3ff7491e8ba6ae2e
Velibor <veliborvasiljevic7@gmail.com>
2020-08-17 09:18:31 +0200

    git ign and app js init

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+//const itemsRoutes = require("./routes/items-routes")
+const HttpError = require("./models/http-error");
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,3 +15,33 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 });
+
+// app.use("/api/items", itemsRoutes)
+
+app.use((req, res, next) => {
+  const err = new HttpError("We can't give you what you are looking for", 404);
+  throw err;
+});
+
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(err.code || 500);
+  res.json({ message: err.message || "Unknown error" });
+});
+
+mongoose
+  .connect(
+    `
+mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.zqjx1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority
+`
+  )
+  .then(() => {
+    console.log("connected to db :)");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log("failed to connect to db");
+    console.log(err);
+  });
