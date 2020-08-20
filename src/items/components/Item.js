1eb254c1a43efbcb5c5d3f240dc3c06af3de3b7e
Velibor <veliborvasiljevic7@gmail.com>
2020-08-20 12:05:39 +0200

    card styling

--- src/items/components/Item.js
+++ src/items/components/Item.js
@@ -3,14 +3,22 @@ import Card from "../../shared/components/UIElements/Card";
 import { Link } from "react-router-dom";
 
 import "./Item.css";
+import cart from "../../shared/icons/cart.svg";
 
 const Item = (props) => {
   return (
     <li className="item__li">
       <Card className="item__card">
         <h2>{props.title}</h2>
-        <p className="i__price">${props.price}</p>
-        <Link to="/cart">Add to cart</Link>
+        <div className="item__bottom">
+          <p className="item__price">${props.price}</p>
+          <Link to="?catergory=skydiving" className="item__category">
+            Skydiving
+          </Link>
+          <Link to="/cart">
+            <img src={cart} alt="Cart" className="item__cart-icon" />
+          </Link>
+        </div>
       </Card>
     </li>
   );
